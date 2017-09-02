a20dd35bea43121a4ada1cbedcc107e792144760
pedrohenriquesanches <pedrohenrique_pi@hotmail.com>
2017-09-02 00:58:00 -0300

    Prototipação de telas: Tela paroquianos iniciada

---
+++ src/main/java/br/com/sgparcat/controllers/MeuBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sgparcat.controllers;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author pedrohensanches
+ */
+
+@ManagedBean
+public class MeuBean {
+    
+    public double getPreco(){
+        return 1000;
+    }
+}
