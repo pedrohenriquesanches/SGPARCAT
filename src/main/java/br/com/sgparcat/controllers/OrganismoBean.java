5f26d616b52c40b5734a879932008b7e08ea78df
pedrohenriquesanches <pedrohenrique_pi@hotmail.com>
2017-10-26 20:33:23 -0200

    Abrir dialog, executar salvar, fechar dialog e redirecionar - ERRO: ao abrir dialog ele redireciona

--- src/main/java/br/com/sgparcat/controllers/OrganismoBean.java
+++ src/main/java/br/com/sgparcat/controllers/OrganismoBean.java
@@ -11,15 +11,20 @@ import br.com.sgparcat.models.Pessoa;
 import br.com.sgparcat.repositories.Organismos;
 import br.com.sgparcat.repositories.Pessoas;
 import br.com.sgparcat.services.OrganismoService;
-import br.com.sgparcat.util.jsf.FacesUtil;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import org.primefaces.event.DragDropEvent;
+import org.primefaces.context.RequestContext;
 
 /**
  *
@@ -55,8 +60,8 @@ public class OrganismoBean implements Serializable {
 
     @PostConstruct
     public void OrganismoBean() {
-        filtrarPessoas();
-        filtrarMembros();
+        listarPessoasQueNaoEstaoNesseOrganismo();
+        listarMembros();
     }
 
     public Organismo getOrganismo() {
@@ -77,15 +82,27 @@ public class OrganismoBean implements Serializable {
 
     public String getInputPesquisaMembro() {
         return inputPesquisaMembro;
-    }    
+    }
 
-    public void salvar() {
-        organismoService.salvar(organismo);
-        FacesUtil.addInfoMessage("O organismo " + organismo.getNome() + " foi criado com sucesso");
+    public void salvar() throws IOException {
+        System.out.println("SALVOU");
+        //organismoService.salvar(organismo);
+        //FacesUtil.addInfoMessage("O organismo " + organismo.getNome() + " foi criado com sucesso");
         //limpar();
     }
     
-    public void filtrarPessoas() {
+    public void fecharDialogoERedirecionar() throws IOException {
+        RequestContext.getCurrentInstance().closeDialog(null);
+        FacesContext.getCurrentInstance().getExternalContext().redirect("adicionar.xhtml");
+    }
+    
+    public String redirecionar(){
+        System.out.println("ENTROU NO METODO REDIRECIONAR");
+        RequestContext.getCurrentInstance().closeDialog(null);
+        return "/organismos/adicionar?faces-redirect=true";
+    }
+
+    public void listarPessoasQueNaoEstaoNesseOrganismo() {
         //Se o organismo esta sendo criado agora, retornar todas as pessoas
         if (organismo.getIdOrganismo() == null) {
             pessoas = repositorioPessoas.retornaTodasAsPessoas();
@@ -94,11 +111,17 @@ public class OrganismoBean implements Serializable {
         }
     }
 
-    public void filtrarMembros() {
+    public void listarMembros() {
         membros = organismo.getMembros();
         if (membros == null) {
             membros = new ArrayList<>();
         }
+    }
+
+    public void PesquisarPessoa() {
+    }
+
+    public void PesquisarMembro() {
     }
 
     public void adicionarMembro(Pessoa pessoa) {
@@ -109,31 +132,41 @@ public class OrganismoBean implements Serializable {
         membros.add(membro);
         pessoas.remove(pessoa);
     }
-    
+
     public void removerMembro(Membro membro) {
         membros.remove(membro);
-        
+
         //atualizar a lista de Pessoas
     }
-    
-    public void onDrop(DragDropEvent ddEvent) {
-        Pessoa pessoa = ((Pessoa) ddEvent.getData());
-        Membro membro = new Membro();
-        membro.setPessoa(pessoa);
-        membro.setOrganismo(organismo);
-        membro.setFuncao(null);
-        membros.add(membro);
-        pessoas.remove(pessoa);
-    }
-    
-    public void teste(){
-        Pessoa pessoa = pessoas.get(2);
-        Membro membro = new Membro();
-        membro.setPessoa(pessoa);
-        membro.setOrganismo(organismo);
-        membro.setFuncao(null);        
-        pessoas.remove(2);
-        membros.add(membro);
+
+    public void preCadastro() {
+        Map<String, Object> options = new HashMap<>();
+        options.put("modal", true);
+        options.put("width", 320);
+        options.put("height", 340);
+        options.put("contentWidth", "100%");
+        options.put("contentHeight", "100%");
+        RequestContext.getCurrentInstance().openDialog("pre-cadastro", options, null);
     }
 
 }
+
+//    public void onDrop(DragDropEvent ddEvent) {
+//        Pessoa pessoa = ((Pessoa) ddEvent.getData());
+//        Membro membro = new Membro();
+//        membro.setPessoa(pessoa);
+//        membro.setOrganismo(organismo);
+//        membro.setFuncao(null);
+//        membros.add(membro);
+//        pessoas.remove(pessoa);
+//    }
+//    
+//    public void teste(){
+//        Pessoa pessoa = pessoas.get(2);
+//        Membro membro = new Membro();
+//        membro.setPessoa(pessoa);
+//        membro.setOrganismo(organismo);
+//        membro.setFuncao(null);        
+//        pessoas.remove(2);
+//        membros.add(membro);
+//    }
